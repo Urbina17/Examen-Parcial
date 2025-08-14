b85c7ec9c52e829793d756c55c920776e26e39d8
Diego <djesusurbinachavez@gmail.com>
2025-08-13 19:51:18 -0600

    Segundo test en rojo

--- src/components/LoginForm/LoginForm.test.jsx
+++ src/components/LoginForm/LoginForm.test.jsx
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import LoginForm from './LoginForm';
 
-test('el botón está deshabilitado al iniciar', () => {
+test('el botón se habilita solo cuando los tres campos están completos', () => {
   render(<LoginForm onLogin={jest.fn()} />);
+  
+  const codigoCliente = screen.getByPlaceholderText(/código de cliente/i);
+  const usuario = screen.getByPlaceholderText(/usuario/i);
+  const contrasena = screen.getByPlaceholderText(/contraseña/i);
   const boton = screen.getByRole('button', { name: /iniciar sesión/i });
+
+  // Antes de llenar campos
   expect(boton).toBeDisabled();
+
+  // No se llenan todos los campos
+  fireEvent.change(codigoCliente, { target: { value: '123' } });
+  fireEvent.change(usuario, { target: { value: 'diego' } });
+  expect(boton).toBeDisabled();
+
+  // Llenar el último campo
+  fireEvent.change(contrasena, { target: { value: '1234' } });
+  expect(boton).toBeEnabled();
 });
+
